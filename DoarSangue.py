f7a56de1e870c6321f2945f0e2584d1f25a0cdde
Rafael Vinicius Garcia <rafael_garcia1993@yahoo.com.br>
2021-08-01 02:11:37 -0300

    CrieiFuncao
    Transformei o código para dentro de uma função.

--- DoarSangue.py
+++ DoarSangue.py
@@ -1,13 +1,22 @@
-nome = str(input('Bom dia, qual o seu nome? '))
-print('Olá senhor(a) {}, bem vindo ao hospital do amor.'.format(nome))
+def doador():
+    nome = str(input('Qual o seu nome senhor(a)?'))
+    print('Meu nome é {}'.format(nome))
+    idade = int(input('Qual a sua idade?'))
+    print('Minha idade é {}'.format(idade))
+    peso = float(input('Digite o seu peso'))
+    print('Meu peso é {}'.format(peso))
+    if idade >= 18 and peso >= 50:
+        print('Muito bem senhor(a) {}, você pode doar sangue.'.format(nome))
+    else:
+        print('Desculpe mas você não pode doar sangue, obrigado pela colaboração!')
+doador()
 
-idade = int(input('Qual sua idade? '))
-print('Muito bem, você tem {} anos. '.format(idade))
 
-peso = float(input('Qual o seu peso? '))
-print('Perfeito, seu peso é de {} Kg. '.format(peso))
 
-if idade >= 18 and peso >= 50:
-    print('Muito bem senhor(a) {}, você pode doar sangue.'.format(nome))
-else:
-    print('Desculpe mas você não pode doar sangue, obrigado pela colaboração!')
+
+
+
+
+
+
+
